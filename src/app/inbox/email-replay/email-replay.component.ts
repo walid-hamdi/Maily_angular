8b23b5ad4644cd45ea75363949318299570e9fb4
walid-hamdi <walidhamdidev@gmail.com>
2023-11-16 09:10:45 +0100

    Formatting email reply

--- src/app/inbox/email-replay/email-replay.component.ts
+++ src/app/inbox/email-replay/email-replay.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Email } from '../email';
-import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-email-replay',
   templateUrl: './email-replay.component.html',
   styleUrls: ['./email-replay.component.css'],
 })
-export class EmailReplayComponent {
+export class EmailReplayComponent implements OnInit {
   showModal = false;
-  email?: Email;
+  @Input() email?: Email;
+
+  ngOnInit(): void {
+    if (this.email)
+      this.email = {
+        ...this.email,
+        to: this.email.from,
+        from: this.email.to,
+        subject: `Reply: ${this.email.subject}`,
+        text: `\n\n\n --------- ${
+          this.email?.from
+        } wrote:\n>${this.email.text.replace(/\n/gi, '\n>')}`,
+      };
+  }
 
   onSubmit(email: Email) {}
 }
