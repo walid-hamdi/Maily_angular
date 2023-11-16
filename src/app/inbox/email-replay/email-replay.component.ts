dae6e631c985c84d02b1eb1f90cc663471c2eaf2
walid-hamdi <walidhamdidev@gmail.com>
2023-11-16 08:58:20 +0100

    Replying to emails

--- src/app/inbox/email-replay/email-replay.component.ts
+++ src/app/inbox/email-replay/email-replay.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Email } from '../email';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-email-replay',
   templateUrl: './email-replay.component.html',
-  styleUrls: ['./email-replay.component.css']
+  styleUrls: ['./email-replay.component.css'],
 })
 export class EmailReplayComponent {
+  showModal = false;
+  email?: Email;
 
+  onSubmit(email: Email) {}
 }
